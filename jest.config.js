44c6a1103f9b5bc4ef8d9148d8f3811367a7c973
Robin Panta <hacktivistic@gmail.com>
2020-05-07 06:43:07 +0530

    fix: custom reporter removed

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 
 module.exports = {
 	verbose: true,
-	coverageReporters: ['text-summary', 'html'],
-	reporters: [
-		'default',
-		['./node_modules/jest-html-reporter', {
-			'pageTitle': 'Test Report'
-		}]
-	]
+	coverageReporters: ['text-summary', 'html']
 }
